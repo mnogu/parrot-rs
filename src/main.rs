ae6875863d48df442365573d9e98301b5b68c8ee
Muneyuki Noguchi <nogu.dev@gmail.com>
2020-09-07 23:00:30 +0900

    Remove spaces

--- src/main.rs
+++ src/main.rs
@@ -170,24 +170,24 @@ cNo.....................................oc
 "#;
 
 static FRAME8: &str = r#"
-                                                  
-                                                  
-                              .ccccc.             
-                         .cc;'coooxkl;.           
-                     .:c:::c:,,,,,;c;;,.'.        
-                   .clc,',:,..:xxocc;'..c;        
-                  .c:,';:ox:..:c,,,,,,...cd,      
-                .c:'.,oxxxxl::l:.,loll;..;ol.     
-                ;Oc..:xxxxxxxxx:.,llll,....oc     
-             .,;,',:loxxxxxxxxx:.,llll;.,,.'ld,   
-            .lo;..:xxxxxxxxxxxx:.'cllc,.:l:'cO;   
-           .:;...'cxxxxxxxxxxxxoc;,::,..cdl;;l'   
-         .cl;':,'';oxxxxxxdxxxxxx:....,cooc,cO;   
-     .,,,::;,lxoc:,,:lxxxxxxxxxxxo:,,;lxxl;'oNc   
-   .cdxo;':lxxxxxxc'';cccccoxxxxxxxxxxxxo,.;lc.   
-  .loc'.'lxxxxxxxxocc;''''';ccoxxxxxxxxx:..oc     
-olc,..',:cccccccccccc:;;;;;;;;:ccccccccc,.'c,     
-Ol;......................................;l'      
+
+
+                              .ccccc.
+                         .cc;'coooxkl;.
+                     .:c:::c:,,,,,;c;;,.'.
+                   .clc,',:,..:xxocc;'..c;
+                  .c:,';:ox:..:c,,,,,,...cd,
+                .c:'.,oxxxxl::l:.,loll;..;ol.
+                ;Oc..:xxxxxxxxx:.,llll,....oc
+             .,;,',:loxxxxxxxxx:.,llll;.,,.'ld,
+            .lo;..:xxxxxxxxxxxx:.'cllc,.:l:'cO;
+           .:;...'cxxxxxxxxxxxxoc;,::,..cdl;;l'
+         .cl;':,'';oxxxxxxdxxxxxx:....,cooc,cO;
+     .,,,::;,lxoc:,,:lxxxxxxxxxxxo:,,;lxxl;'oNc
+   .cdxo;':lxxxxxxc'';cccccoxxxxxxxxxxxxo,.;lc.
+  .loc'.'lxxxxxxxxocc;''''';ccoxxxxxxxxx:..oc
+olc,..',:cccccccccccc:;;;;;;;;:ccccccccc,.'c,
+Ol;......................................;l'
 "#;
 
 static FRAME9: &str = r#"
